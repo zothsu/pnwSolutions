f81b52c42652f7e6370d9309ea36eee6447542e5
Yongkie Wiyogo <4080600+ywiyogo@users.noreply.github.com>
2025-04-13 14:38:20 +0800

    Upgrade Astro to 5.6.1 and TailwindCSS to 4.1.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [
-    tailwind(),
     alpinejs()
   ]
 });
