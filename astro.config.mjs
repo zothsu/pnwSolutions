582a01a01a46b1ba70e185b8684384bc52df44f6
yujian(larry) xue <azoux0311@gmail.com>
2025-06-20 17:12:23 +0800

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   integrations: [
+    tailwind(),
     alpinejs()
   ]
 });
