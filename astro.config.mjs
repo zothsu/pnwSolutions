2083f9c7dc167d942820989224f93a864a6b6dce
Zoe Sullivan <zoetsullivan@gmail.com>
2025-09-26 11:16:42 -0700

    add sitemap to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import alpinejs from '@astrojs/alpinejs';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    alpinejs()
-  ],
+  integrations: [alpinejs(), sitemap()],
 
   vite: {
     plugins: [
